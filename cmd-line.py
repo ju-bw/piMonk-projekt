21aa5a51c2f49200ea77e0d40bcab4e072f67875
Jan Unger <info@bw1.eu>
2018-11-04 17:14:27 +0100

    python-grundlagen

--- cmd-line.py
+++ cmd-line.py
@@ -1,6 +1,8 @@
 import sys
 
-for (i, value) in enumerate(sys.argv):
-  print("arg: %d %s " % (i, value))
+try:
+  for (i, value) in enumerate(sys.argv):
+    print("arg: %d %s " % (i, value))
+except:
+  print("$ python3 cmd-line.py a b c")
 
-# $ python3 cmd-line.py a b c
--- webserver-test.py
+++ webserver-test.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 @route('/')
 def index(name='time'):
-dt = datetime.now()
-time = "{:%Y-%m-%d %H:%M:%S}".format(dt)
-return template('<b>Pi thinks the date/time is: {{t}}</b>', t=time)
+  dt = datetime.now()
+  time = "{:%Y-%m-%d %H:%M:%S}".format(dt)
+  return template('<b>Pi thinks the date/time is: {{t}}</b>', t=time)
+  
 run(host='192.168.1.16', port=80)
